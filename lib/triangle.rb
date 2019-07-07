b3e0d1b7bcfb7cda2ce4b661c8a2c47b39f6254a
ghatzid <ghatzid@gmail.com>
2019-07-07 17:36:03 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :ang1, :ang2, :ang3
+  @@all = []
+  def initialize(ang1, ang2, ang3)
+    @ang1 = ang1
+    @ang2 = ang2
+    @ang3 = ang3
+  end
+
+  def kind
+    is_it_real
+    if self.ang1 == self.ang2 && self.ang1 == self.ang3
+      :equilateral
+    elsif self.ang1 == self.ang2 || self.ang1 == self.ang3 || self.ang2 == self.ang3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  def is_it_real
+    raise TriangleError if 
+    self.ang1 + self.ang2 <= self.ang3 || 
+    self.ang1 + self.ang3 <= self.ang2 || 
+    self.ang3 + self.ang2 <= self.ang1 ||
+    self.ang1 <= 0 || self.ang2 <= 0 || self.ang3 <= 0
+  end
+
+  class TriangleError < StandardError
+  end
 end
